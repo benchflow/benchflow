4be76a73a0fe9379cb08a4833a5a4e8bc486890a
Vincenzo Ferme <fermevincenzo@gmail.com>
2015-12-25 16:32:17 +0100

    Generalised for all the repos

--- ci/clean_v-dev_release.sh
+++ ci/clean_v-dev_release.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 # Clean the v-dev release and tag we are using to release development version in CI
+# Meants to: be used in Travis-CI
+# Accepts Envs:
+#	- REPO_NAME: the name of the repository under the benchflow user, without leading /
+#	- GITHUB_ACCESS_TOKEN: the access token used to interact with git (generally stored in Travis)
 set -ex
 # install before_deploy dependencies (JSON parser)
 sudo wget https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 -O /usr/bin/jq
@@ -10,11 +14,11 @@ git config --global user.name "Travis CI"
 # configure tag (prepended v- in order to ignore the build on push)
 export GIT_TAG=v-dev
 # delete the release, as well as its assets, if it exists
-export id=$(curl -sS -u x-oauth-basic:$GITHUB_ACCESS_TOKEN -X GET https://api.github.com/repos/benchflow/faban/releases/tags/$GIT_TAG | /usr/bin/jq '.id')
-if [ "$id" != "null" ]; then curl -u x-oauth-basic:$GITHUB_ACCESS_TOKEN -X DELETE https://api.github.com/repos/benchflow/faban/releases/$id; fi
+export id=$(curl -sS -u x-oauth-basic:$GITHUB_ACCESS_TOKEN -X GET https://api.github.com/repos/benchflow/$REPO_NAME/releases/tags/$GIT_TAG | /usr/bin/jq '.id')
+if [ "$id" != "null" ]; then curl -u x-oauth-basic:$GITHUB_ACCESS_TOKEN -X DELETE https://api.github.com/repos/benchflow/$REPO_NAME/releases/$id; fi
 # delete tag if it already exists
 git tag | if [ $(grep -c $GIT_TAG) -eq 1 ]; then git tag -d $GIT_TAG; fi
-git push -q https://$GITHUB_ACCESS_TOKEN:x-oauth-basic@github.com/benchflow/faban :refs/tags/$GIT_TAG
+git push -q https://$GITHUB_ACCESS_TOKEN:x-oauth-basic@github.com/benchflow/$REPO_NAME :refs/tags/$GIT_TAG
 # push the new tag
 git tag $GIT_TAG -a -m "Development Tag"
-git push -q https://$GITHUB_ACCESS_TOKEN:x-oauth-basic@github.com/benchflow/faban --tags
+git push -q https://$GITHUB_ACCESS_TOKEN:x-oauth-basic@github.com/benchflow/$REPO_NAME --tags
