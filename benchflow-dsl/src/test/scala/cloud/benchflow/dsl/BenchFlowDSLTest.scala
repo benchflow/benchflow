f517a011d1583da60c23a0d269a3e93891f3932f
Jesper Findahl <jesper.findahl@gmail.com>
2017-03-19 11:44:55 +0100

    Adds link to JUnit 4 in scala.

--- benchflow-dsl/src/test/scala/cloud/benchflow/dsl/BenchFlowDSLTest.scala
+++ benchflow-dsl/src/test/scala/cloud/benchflow/dsl/BenchFlowDSLTest.scala
@@ -8,6 +8,8 @@ import org.scalatest.junit.JUnitSuite
 import scala.io.Source
 
 /**
+  * based on http://www.scalatest.org/getting_started_with_junit_4_in_scala
+  *
   * @author Jesper Findahl (jesper.findahl@usi.ch)
   *         created on 16.03.17.
   */
