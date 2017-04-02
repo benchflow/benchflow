c1f6316a725cee964845117f68357177d9d5d642
Vincenzo Ferme <fermevincenzo@gmail.com>
2017-04-02 19:24:35 +0200

    Adds the first working draft of the single repo projects build trigger

--- devops/diffs-check.sh
+++
@@ -1,8 +0,0 @@
-changed_folders=$(curl -sS https://api.github.com/repos/$WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY/commits/$WERCKER_GIT_COMMIT | jq '.files[] | select(.filename | split ("/") | length > 1 ) | .filename | split ("/") | .[0]' | uniq)
-
-changed_folders_arr=(${changed_folders// / })
-
-for folder in "${changed_folders_arr[@]}"
-do
-  echo "$folder"
-done
