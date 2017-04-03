feb822cb790e9f843a9099645b1100194bc14ba2
Vincenzo Ferme <fermevincenzo@gmail.com>
2017-04-03 09:34:57 +0200

    Fixes path reference

--- devops/deploy-on-bintray.sh
+++ devops/deploy-on-bintray.sh
@@ -10,7 +10,7 @@ echo "Creating version for branch..."
 curl -v -u$BINTRAY_ADMIN_USERNAME:$BINTRAY_API_KEY -H "Content-Type: application/json" -X POST -d '{"name": "'"$WERCKER_GIT_BRANCH"'", "desc": "'"$WERCKER_GIT_BRANCH"'"}' "https://api.bintray.com/packages/$BINTRAY_USERNAME/$BINTRAY_USERNAME-maven/$BINTRAY_USERNAME-dsl/versions" | jq .
 echo ""
 echo "Uploading artefact for commit..."
-curl -T benchflow-dsl.jar -u$BINTRAY_ADMIN_USERNAME:$BINTRAY_API_KEY "https://api.bintray.com/content/$BINTRAY_USERNAME/$BINTRAY_USERNAME-maven/$BINTRAY_USERNAME-dsl/$WERCKER_GIT_BRANCH_$CUSTOM_VERSION_TAG/benchflow-dsl_$WERCKER_GIT_BRANCH_$CUSTOM_VERSION_TAG.jar?publish=1&override=1" | jq .
+curl -T $WERCKER_SOURCE_DIR/benchflow-dsl/target/benchflow-dsl.jar -u$BINTRAY_ADMIN_USERNAME:$BINTRAY_API_KEY "https://api.bintray.com/content/$BINTRAY_USERNAME/$BINTRAY_USERNAME-maven/$BINTRAY_USERNAME-dsl/$WERCKER_GIT_BRANCH_$CUSTOM_VERSION_TAG/benchflow-dsl_$WERCKER_GIT_BRANCH_$CUSTOM_VERSION_TAG.jar?publish=1&override=1" | jq .
 echo ""
 echo "Uploading artefact for branch..."
-curl -T benchflow-dsl.jar -u$BINTRAY_ADMIN_USERNAME:$BINTRAY_API_KEY "https://api.bintray.com/content/$BINTRAY_USERNAME/$BINTRAY_USERNAME-maven/$BINTRAY_USERNAME-dsl/$WERCKER_GIT_BRANCH/benchflow-dsl_$WERCKER_GIT_BRANCH.jar?publish=1&override=1" | jq .
+curl -T $WERCKER_SOURCE_DIR/benchflow-dsl/target/benchflow-dsl.jar -u$BINTRAY_ADMIN_USERNAME:$BINTRAY_API_KEY "https://api.bintray.com/content/$BINTRAY_USERNAME/$BINTRAY_USERNAME-maven/$BINTRAY_USERNAME-dsl/$WERCKER_GIT_BRANCH/benchflow-dsl_$WERCKER_GIT_BRANCH.jar?publish=1&override=1" | jq .
