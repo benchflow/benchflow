ba9e75363b5e09f4dc4ab980b4e38816dfdcaa56
Jesper Findahl <jesper.findahl@gmail.com>
2017-04-06 15:19:34 +0200

    Adds comments to clarify where things happens, and how the code converts a Test to an Experiment taking advantage of the MoultingYAML library.

--- benchflow-dsl/src/main/scala/cloud/benchflow/dsl/BenchFlowDSL.scala
+++ benchflow-dsl/src/main/scala/cloud/benchflow/dsl/BenchFlowDSL.scala
@@ -1,8 +1,8 @@
 package cloud.benchflow.dsl
 
-import cloud.benchflow.dsl.definition.{ BenchFlowExperiment, BenchFlowTest }
-import cloud.benchflow.dsl.definition.BenchFlowTestYamlProtocol._
 import cloud.benchflow.dsl.definition.BenchFlowExperimentYamlProtocol._
+import cloud.benchflow.dsl.definition.BenchFlowTestYamlProtocol._
+import cloud.benchflow.dsl.definition.{ BenchFlowExperiment, BenchFlowTest }
 import net.jcazevedo.moultingyaml._
 
 import scala.util.Try
@@ -12,6 +12,10 @@ import scala.util.Try
  *         created on 10.03.17.
  */
 object BenchFlowDSL {
+
+  /**
+   * To serialize/deserialize YAML this library uses https://github.com/jcazevedo/moultingyaml.
+   */
 
   def testFromYaml(testDefinitionYaml: String): Try[BenchFlowTest] = {
 
@@ -44,9 +48,22 @@ object BenchFlowDSL {
 
   // TODO - add methods for common operations/changes to tests/experiments
 
+  /**
+   * Generates a BenchFlowExperiment from a BenchFlow Test Definition YAML string with
+   * values equal to those in the YAML string.
+   *
+   * Will return a failure if the Test Definition is invalid.
+   *
+   * @param testDefinitionYaml
+   * @return
+   */
   def experimentFromTestYaml(testDefinitionYaml: String): Try[BenchFlowExperiment] = {
 
     // convert to experiment and validate syntax
+    // NOTE: Since the key-value pairs in an experiment definition is a subset of those
+    // found in a test, we can use the same YAML protocol code to parse from a test definition
+    // to an experiment, as from an experiment definition to an experiment. Key-value pairs that
+    // should not be in an experiment will simply be ignored.
     val experiment: Try[BenchFlowExperiment] = testDefinitionYaml.parseYaml.convertTo[Try[BenchFlowExperiment]]
 
     // TODO - validate semantic in separate function on the object
@@ -55,6 +72,14 @@ object BenchFlowDSL {
 
   }
 
+  /**
+   * Generates a BenchFlowExperiment from a BenchFlow Experiment Definition YAML string.
+   *
+   * Will return a failure if the Experiment Definition is invalid.
+   *
+   * @param experimentDefinitionYaml
+   * @return
+   */
   def experimentFromExperimentYaml(experimentDefinitionYaml: String): Try[BenchFlowExperiment] = {
 
     val experiment: Try[BenchFlowExperiment] = experimentDefinitionYaml.parseYaml.convertTo[Try[BenchFlowExperiment]]
@@ -65,6 +90,12 @@ object BenchFlowDSL {
 
   }
 
+  /**
+   * Generates a YAML string from a BenchFlowExperiment.
+   *
+   * @param benchFlowExperiment
+   * @return
+   */
   def experimentToYamlString(benchFlowExperiment: BenchFlowExperiment): String = {
 
     // TODO - validate semantic in separate function on the object
