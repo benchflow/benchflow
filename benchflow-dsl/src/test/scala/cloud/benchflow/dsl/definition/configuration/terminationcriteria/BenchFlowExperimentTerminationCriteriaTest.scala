68ff3e0f16834fcdf05a6d33f7b7765d8fdc3c58
Jesper Findahl <jesper.findahl@gmail.com>
2017-04-17 23:56:59 +0200

    Adds tests for termination criteria.

---
+++ benchflow-dsl/src/test/scala/cloud/benchflow/dsl/definition/configuration/terminationcriteria/BenchFlowExperimentTerminationCriteriaTest.scala
@@ -0,0 +1,31 @@
+package cloud.benchflow.dsl.definition.configuration.terminationcriteria
+
+import cloud.benchflow.dsl.definition.configuration.terminationcriteria.BenchFlowExperimentTerminationCriteriaYamlProtocol._
+import net.jcazevedo.moultingyaml._
+import org.junit.{Assert, Test}
+import org.scalatest.junit.JUnitSuite
+
+import scala.util.Try
+
+/**
+ * @author Jesper Findahl (jesper.findahl@usi.ch)
+ *         created on 2017-04-17
+ */
+class BenchFlowExperimentTerminationCriteriaTest extends JUnitSuite {
+
+  private val completeTerminationCriteriaYaml: String =
+    """
+      |experiment:
+      | type: fixed
+      | number: 5
+    """.stripMargin
+
+  @Test def completeTerminationCriteria(): Unit = {
+
+    val terminationCriteria = completeTerminationCriteriaYaml.parseYaml.convertTo[Try[BenchFlowExperimentTerminationCriteria]]
+
+    Assert.assertTrue(terminationCriteria.isSuccess)
+
+  }
+
+}
