03e8d008b8819a7a69a6042a2ba35a847e609bb2
Jesper Findahl <jesper.findahl@gmail.com>
2017-04-18 13:26:17 +0200

    Includes files from benchflow/experiments-manager repo.

---
+++ benchflow-experiment-manager/application/src/main/java/cloud/benchflow/experimentmanager/constants/BenchFlowConstants.java
@@ -0,0 +1,73 @@
+package cloud.benchflow.experimentmanager.constants;
+
+/**
+ * @author Jesper Findahl (jesper.findahl@usi.ch)
+ *         created on 2017-03-21
+ */
+public class BenchFlowConstants {
+
+    // REST API
+    public static final String VERSION_1_PATH = "/v1";
+    public static final String USERS_PATH = "/users/";
+    public static final String TESTS_PATH = "/tests/";
+    public static final String EXPERIMENTS_PATH = "/experiments/";
+    public static final String TRIALS_PATH = "/trials/";
+    // Minio
+    public static final String DEPLOYMENT_DESCRIPTOR_NAME = "docker-compose";
+    public static final String PT_PE_DEFINITION_NAME = "benchflow-test";
+    public static final String BPMN_MODELS_FOLDER_NAME = "models";
+    public static final String GENERATED_BENCHMARK_FILENAME = "benchflow-benchmark.jar";
+    public static final String FABAN_CONFIG_FILENAME = "run.xml";
+    public static final String MINIO_ID_DELIMITER = "/";
+    // TODO - is this correct with only one bucket and it is called tests? Maybe better then just 'benchflow'?
+    public static final String TESTS_BUCKET = "tests";
+    // MongoDB
+    public static final String DB_NAME = "benchflow-experiment-manager";
+    public static final String MODEL_ID_DELIMITER = ".";
+    public static final String MODEL_ID_DELIMITER_REGEX = "\\.";
+    // Faban
+    public static final String FABAN_ID_DELIMITER = "_";
+    // TODO - put in common library so they can be handled by client
+    // Exceptions
+    public static final String INVALID_EXPERIMENT_ID_MESSAGE = "Invalid Experiment ID";
+    private static final String YAML_EXTENSION = ".yml";
+    public static final String PT_PE_DEFINITION_FILE_NAME = PT_PE_DEFINITION_NAME + YAML_EXTENSION;
+    public static final String DEPLOYMENT_DESCRIPTOR_FILE_NAME = DEPLOYMENT_DESCRIPTOR_NAME + YAML_EXTENSION;
+
+    public static String getExperimentID(String username, String testName, int testNumber, int experimentNumber) {
+        return username + MODEL_ID_DELIMITER + testName + MODEL_ID_DELIMITER + testNumber + MODEL_ID_DELIMITER + experimentNumber;
+    }
+
+    public static String getPathFromExperimentID(String experimentID) {
+
+        String[] experimentIDArray = experimentID.split(MODEL_ID_DELIMITER_REGEX);
+        String username = experimentIDArray[0];
+        String testName = experimentIDArray[1];
+        String testNumber = experimentIDArray[2];
+        String experimentNumber = experimentIDArray[3];
+
+        return VERSION_1_PATH
+                + USERS_PATH + username
+                + TESTS_PATH + testName + "/" + testNumber
+                + EXPERIMENTS_PATH + experimentNumber;
+
+    }
+
+    public static String getPathFromTrialID(String trialID) {
+
+        String[] trialIDArray = trialID.split(MODEL_ID_DELIMITER_REGEX);
+        String username = trialIDArray[0];
+        String testName = trialIDArray[1];
+        String testNumber = trialIDArray[2];
+        String experimentNumber = trialIDArray[3];
+        String trialNumber = trialIDArray[4];
+
+        return VERSION_1_PATH
+                + USERS_PATH + username
+                + TESTS_PATH + testName + "/" + testNumber
+                + EXPERIMENTS_PATH + experimentNumber
+                + TRIALS_PATH + trialNumber;
+
+    }
+
+}
---
+++ benchflow-experiment-manager/application/src/main/java/cloud/benchflow/experimentmanager/exceptions/web/NoSuchExperimentIdException.java
@@ -0,0 +1,16 @@
+package cloud.benchflow.experimentmanager.exceptions.web;
+
+import javax.ws.rs.WebApplicationException;
+
+/**
+ * @author Simone D'Avico (simonedavico@gmail.com)
+ *         <p/>
+ *         Created on 09/04/16.
+ */
+public class NoSuchExperimentIdException extends WebApplicationException {
+
+    public NoSuchExperimentIdException(String experimentId) {
+        super("PerformanceExperimentModel Id " + experimentId + " does not exist.");
+    }
+
+}
