306cfb8a658774e854a42b007a7b55d4f6b96c4f
Vincenzo Ferme <fermevincenzo@gmail.com>
2017-04-19 15:26:09 +0200

    Converts the base image to the new building system

---
+++ docker-images/base-images/base/build-image.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+# TODO: remove, when the code become stable
+set -xv	
+
+# Remove sample application from chaperone.d
+rm /apps/chaperone.d/200-userapp.conf
+rm /apps/bin/sample_app
+
+# create the directory that is going to accomodate the app
+mkdir -p /app
+# enable chaperone to work on the following directories
+chown -R runapps: /apps /app
