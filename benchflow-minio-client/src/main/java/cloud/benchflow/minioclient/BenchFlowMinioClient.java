edf88a97031d9830c617c3a308bbf3f1edb6d9ab
Jesper Findahl <jesper.findahl@gmail.com>
2017-04-19 10:30:34 +0200

    Adds the initial files for the project.

---
+++ benchflow-minio-client/src/main/java/cloud/benchflow/minioclient/BenchFlowMinioClient.java
@@ -0,0 +1,12 @@
+package cloud.benchflow.minioclient;
+
+/**
+ * @author Jesper Findahl (jesper.findahl@usi.ch)
+ *         created on 2017-04-19
+ */
+abstract public class BenchFlowMinioClient {
+
+
+
+
+}
