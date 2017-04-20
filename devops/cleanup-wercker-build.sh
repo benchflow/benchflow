a6f01609278bd610935e57a97b6b80a3d1f3d5e6
Vincenzo Ferme <fermevincenzo@gmail.com>
2017-04-20 16:23:50 +0200

    Updates the way we detect if we are in the Wercker Cloud

--- devops/cleanup-wercker-build.sh
+++ devops/cleanup-wercker-build.sh
@@ -6,9 +6,11 @@
 set -xv	
 
 # Determine if we are in a CI environment (for now the Wercker Cloud) or not
-if [ "$WERCKER" = true ]; then
-	# Reference for the - before rm: https://superuser.com/a/523510
-	-rm -Rf /report /pipeline
+# Although Wercker suggests to use the CI or WERCKER variables (http://devcenter.wercker.com/docs/environment-variables/available-env-vars)
+# , those are true also locally, somehow. WERCKER_STARTED_BY is only set in the Wercker Cloud
+if [ -z ${WERCKER_STARTED_BY+x} ]; then
+	# Reference for the - before rm: https://superuser.com/a/523510 (It does not work)
+	rm -Rf /report /pipeline
 else
 	echo 'Skipping step, because not needed for local execution'
 fi
