3dec596df332ccd600da6fd09a1be7a6b4430b1e
Vincenzo Ferme <fermevincenzo@gmail.com>
2017-04-21 17:21:07 +0200

    Renames service configuration file

--- benchflow-experiment-manager/build-image.sh
+++ benchflow-experiment-manager/build-image.sh
@@ -8,6 +8,6 @@ set -xv
 cp configuration.yml /app/
 
 # copy resources (experiment configurations)
-cp -a services/300-experiments-manager.conf /apps/chaperone.d/300-experiments-manager.conf
+cp -a services/300-experiment-manager.conf /apps/chaperone.d/300-experiment-manager.conf
 
 cp -a services/envcp/config.tpl /app/config.tpl
