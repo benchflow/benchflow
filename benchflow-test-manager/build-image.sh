95cbf8a7cbc4c0c102c7fc2f6eb38655abf81119
Vincenzo Ferme <fermevincenzo@gmail.com>
2017-04-21 17:20:49 +0200

    Adds new build system

---
+++ benchflow-test-manager/build-image.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+# Build and Configure the BenchFlow Experiment Manager Service
+
+# TODO: remove, when the code become stable
+set -xv
+
+# # copy service configuration
+cp configuration.yml /app/
+
+# # copy resources (experiment configurations)
+cp -a services/300-test-manager.conf /apps/chaperone.d/300-test-manager.conf
+
+cp -a services/envcp/config.tpl /app/config.tpl
