d6c30f75fb2bc0ad1e05a812d340fa2bc69a9416
Jesper Findahl <jeppe-style@users.noreply.github.com>
2017-04-23 20:33:27 +0200

    Adds (exploration) strategy section to DSL (#250)
    * Updates versions in POM. Adds test for ExperimentTerminationCriteria. Moves previous tests into ‘simone’ subfolder.
    
    * Adds tests for termination criteria.
    
    * Adds support for goal.exploration_space.workload.users
    
    * Adds method to generate experiment from test and parameters (work-in-progress).
    
    * Adds functionality to generate experiment YAML from test YAML with changing number of users.
    
    * Updates error handling so that exceptions are thrown outside the library and Try’s are used inside.
    
    * Small simplification using string interpolation
    
    This change serves as an example of the use of string interpolation.
    In some cases it may make your code more clear.
    
    * Replace head/tail for take/drop to avoid exce

---
+++ benchflow-dsl/src/main/scala/cloud/benchflow/dsl/definition/configuration/strategy/validation/ValidationStrategyType.scala
@@ -0,0 +1,10 @@
+package cloud.benchflow.dsl.definition.configuration.strategy.validation
+
+/**
+ * @author Jesper Findahl (jesper.findahl@usi.ch)
+ *         created on 2017-04-21
+ */
+object ValidationStrategyType extends Enumeration {
+  type ValidationStrategyType = Value
+  val RandomValidationSet = Value("random-validation-set")
+}
