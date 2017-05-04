4708f29329601fbe9a953e78aca237ec52e811cf
Jesper Findahl <jesper.findahl@gmail.com>
2017-05-04 12:48:08 +0200

    Moves complete exploration users test to tests folder. Changes goal type from load to exploration.

--- benchflow-test-manager/application/src/test/java/cloud/benchflow/testmanager/helpers/TestFiles.java
+++ benchflow-test-manager/application/src/test/java/cloud/benchflow/testmanager/helpers/TestFiles.java
@@ -10,7 +10,9 @@ import java.io.InputStream;
  */
 public class TestFiles {
 
-    private static String TEST_EXPLORATION_COMPLETE_USERS_FILE = "src/test/resources/data/exploration/complete-users/benchflow-test.yml";
+    private static String TESTS_FOLDER = "../../tests/data/dsl-examples/definition/benchflow-test/";
+
+    private static String TEST_EXPLORATION_COMPLETE_USERS_FILE = TESTS_FOLDER + "exploration/complete/users/benchflow-test.yml";
 
     public static InputStream getTestExplorationCompleteUsersInputStream() throws FileNotFoundException {
 
