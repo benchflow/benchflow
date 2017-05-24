3e8da59136114d7375961bcc7967a12e5fbb8c44
Jesper Findahl <jesper.findahl@gmail.com>
2017-05-24 23:09:47 +0200

    Removes unused files

--- benchflow-dsl/src/main/scala/cloud/benchflow/dsl/definition/simone/binding/Binding.scala
+++
@@ -1,9 +0,0 @@
-package cloud.benchflow.dsl.definition.simone.binding
-
-import cloud.benchflow.dsl.definition.simone.properties.Properties
-
-/**
- * @author Jesper Findahl (jesper.findahl@usi.ch)
- *         created on 13.03.17.
- */
-case class Binding(boundService: String, config: Option[Properties])
