620517f32dc686a04f968352072c4a968ee4e11c
Vincenzo Ferme <fermevincenzo@gmail.com>
2017-06-20 15:22:20 +0200

    Adds support for controller URL

--- benchflow-faban-client/src/main/java/cloud/benchflow/faban/client/configurations/FabanClientConfigImpl.java
+++ benchflow-faban-client/src/main/java/cloud/benchflow/faban/client/configurations/FabanClientConfigImpl.java
@@ -1,19 +1,22 @@
 package cloud.benchflow.faban.client.configurations;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * A configuration class for the faban client.
  * The user can configure username and password,
  * or username, password and masterURL.
  *
- * @author Simone D'Avico (simonedavico@gmail.com)
+ * @author Simone D'Avico (simonedavico@gmail.com)\
+ * @author vincenzoferme
  */
 public class FabanClientConfigImpl implements FabanClientConfig {
 
   private final String user;
   private final String password;
   private final URI masterURL;
+  private final URI controllerURL;
 
   /**
    * Creates a FabanClientConfigImpl on the default url.
@@ -36,6 +39,19 @@ public class FabanClientConfigImpl implements FabanClientConfig {
     this.user = user;
     this.password = password;
     this.masterURL = masterURL;
+
+    URI controllerURL = null;
+
+    try {
+      controllerURL = new URI(masterURL + "/controller");
+    } catch (URISyntaxException e) {
+      System.err.println("There was a problem initializing the default "
+          + "faban client configuration. See the stack" + "trace for more informations.");
+      e.printStackTrace();
+    }
+
+    this.controllerURL = controllerURL;
+
   }
 
 
@@ -49,5 +65,9 @@ public class FabanClientConfigImpl implements FabanClientConfig {
 
   public URI getMasterURL() {
     return masterURL;
+  }
+
+  public URI getControllerURL() {
+    return controllerURL;
   }
 }
