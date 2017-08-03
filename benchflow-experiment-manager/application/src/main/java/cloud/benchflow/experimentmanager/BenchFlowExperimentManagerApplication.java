8917b33a6c589eb812b0a5ab7226996b563649b7
Vincenzo Ferme <fermevincenzo@gmail.com>
2017-08-03 10:32:39 +0200

    Removes Extra http from Faban URL

--- benchflow-experiment-manager/application/src/main/java/cloud/benchflow/experimentmanager/BenchFlowExperimentManagerApplication.java
+++ benchflow-experiment-manager/application/src/main/java/cloud/benchflow/experimentmanager/BenchFlowExperimentManagerApplication.java
@@ -138,7 +138,7 @@ public class BenchFlowExperimentManagerApplication
 
     // external services addresses
     minioServiceAddress = configuration.getMinioServiceFactory().getAddress();
-    fabanManagerServiceAddress = "http://" + configuration.getFabanServiceFactory().getAddress();
+    fabanManagerServiceAddress = configuration.getFabanServiceFactory().getAddress();
 
     final Client client = new JerseyClientBuilder(environment)
         .using(configuration.getJerseyClientConfiguration()).build(environment.getName());
