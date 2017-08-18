0529c5375e083fa2c16ebe026594f43fa5dff489
Jesper Findahl <jesper.findahl@gmail.com>
2017-08-18 10:24:00 +0200

    Fixes code style issues

--- benchflow-dsl/src/test/scala/cloud/benchflow/dsl/package.scala
+++ benchflow-dsl/src/test/scala/cloud/benchflow/dsl/package.scala
@@ -1,9 +1,9 @@
 package cloud.benchflow
 
 /**
- * @author Jesper Findahl (jesper.findahl@usi.ch)
- *         created on 11.03.17.
- */
+  * @author Jesper Findahl (jesper.findahl@usi.ch)
+  *         created on 11.03.17.
+  */
 package object dsl {
 
   private val BenchFlowTestExamplesFolder = "../tests/data/dsl-examples/definition/benchflow-test/"
@@ -13,9 +13,24 @@ package object dsl {
   private val exhaustiveFolder = "exhaustive_exploration/"
   private val oneAtATimeFolder = "one-at-a-time/"
 
-  val BenchFlowExhaustiveExplorationUsersExample: String = BenchFlowTestExamplesFolder + exhaustiveFolder + oneAtATimeFolder + "users/benchflow-test.yml"
-  val BenchFlowExhaustiveExplorationUsersMemoryEnvironmentExample: String = BenchFlowTestExamplesFolder + exhaustiveFolder + oneAtATimeFolder + "users-memory-environment/benchflow-test.yml"
-  val BenchFlowExhaustiveExplorationMemoryExample: String = BenchFlowTestExamplesFolder + exhaustiveFolder + oneAtATimeFolder + "memory/benchflow-test.yml"
-  val BenchFlowExhaustiveExplorationUsersEnvironmentExample: String = BenchFlowTestExamplesFolder + exhaustiveFolder + oneAtATimeFolder + "users-environment/benchflow-test.yml"
+  val BenchFlowExhaustiveExplorationUsersExample: String = BenchFlowTestExamplesFolder +
+    exhaustiveFolder +
+    oneAtATimeFolder +
+    "users/benchflow-test.yml"
+
+  val BenchFlowExhaustiveExplorationUsersMemoryEnvironmentExample: String = BenchFlowTestExamplesFolder +
+    exhaustiveFolder +
+    oneAtATimeFolder +
+    "users-memory-environment/benchflow-test.yml"
+
+  val BenchFlowExhaustiveExplorationMemoryExample: String = BenchFlowTestExamplesFolder +
+    exhaustiveFolder +
+    oneAtATimeFolder +
+    "memory/benchflow-test.yml"
+
+  val BenchFlowExhaustiveExplorationUsersEnvironmentExample: String = BenchFlowTestExamplesFolder +
+    exhaustiveFolder +
+    oneAtATimeFolder +
+    "users-environment/benchflow-test.yml"
 
 }
